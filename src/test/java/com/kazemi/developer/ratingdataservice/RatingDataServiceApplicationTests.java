83ba1d91ba2f92d5f7d6156afcdecde103d10997
Saeid Kazemi <becomming.fullstack.4@gmail.com>
2020-07-26 21:36:37 +0430

    Initial

---
+++ src/test/java/com/kazemi/developer/ratingdataservice/RatingDataServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.kazemi.developer.ratingdataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+
+class RatingDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+//		Asser
+	}
+
+}
